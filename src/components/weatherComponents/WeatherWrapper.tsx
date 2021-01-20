f27ed1ffe8451f982e41c19d3f48690c7bcb8dc1
blazej-k <blazejkania1@gmail.com>
2021-01-20 21:54:20 +0100

    correct fetching data

--- src/components/weatherComponents/WeatherWrapper.tsx
+++ src/components/weatherComponents/WeatherWrapper.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import * as env from 'process'
 import Browser from './search/Browser'
 
- 
+
 const WeatherWrapper: FC = () => {
 
     const [cityName, setCityName] = useState('')
@@ -10,36 +10,38 @@ const WeatherWrapper: FC = () => {
     let ENDPOINT = process.env.API_KEY
 
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-        const {value} = e.target
+        const { value } = e.target
         setCityName(value)
     }
 
-    const getWeather = async() => {
-        const res = await fetch(`${ENDPOINT}${cityName}`)
-        .then(res => {
-            if (!res.ok) {
-                setWeather({})
-                throw new Error('Wrong city');
-            }
-            return res;
-          })
-        .then(res => res.json())
-        .then(res => setWeather(res))
-        .catch((err) => {
-            console.log(err)
-        })
+    const getWeather = async () => {
+        const URL = `${ENDPOINT}${cityName.length > 1 ? cityName[0].toUpperCase() + cityName.slice(1, cityName.length) :
+            cityName[0].toUpperCase()}`
+        await fetch(URL)
+            .then(res => {
+                if (!res.ok) {
+                    setWeather({})
+                    throw new Error('Wrong city');
+                }
+                return res;
+            })
+            .then(res => res.json())
+            .then(res => setWeather(res))
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     useEffect(() => {
-        getWeather()
+        cityName.length > 0 ? getWeather() : setWeather({})
     }, [cityName])
 
     return (
         <>
-        <Browser cityName={cityName} handleInputChange={handleInput}/>
-        {weather?.main?.temp && <h1>{cityName}: {weather.main.temp}</h1>}
+            <Browser cityName={cityName} handleInputChange={handleInput} />
+            {weather?.main?.temp && <h1>{weather.name}: {weather.main.temp}</h1>}
         </>
     );
 }
- 
+
 export default WeatherWrapper;
