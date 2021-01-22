f72c6487279d1f6d4f854dda7386130db5c52918
blazej-k <blazejkania1@gmail.com>
2021-01-22 19:36:37 +0100

    create weather state in weatherWrapper

--- src/components/weatherComponents/WeatherWrapper.tsx
+++ src/components/weatherComponents/WeatherWrapper.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import * as env from 'process'
 import Browser from './search/Browser'
-import {Subject} from 'rxjs'
+import { Subject } from 'rxjs'
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 const subject = new Subject<string>();
@@ -10,15 +10,26 @@ const subscription = subject.pipe(
     distinctUntilChanged()
 )
 
+interface State{
+    loading: boolean,
+    error: boolean,
+    weather: any
+}
+
+const initState: State = {
+    loading: false,
+    error: false,
+    weather: {}
+}
 
 const WeatherWrapper: FC = () => {
 
     const [cityName, setCityName] = useState('')
-    const [weather, setWeather] = useState<any>({})
-    const [loadnig, setLoadnig] = useState(false)
+    const [weatherState, setWeatherState] = useState(initState)
     let ENDPOINT = process.env.API_KEY
 
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        weatherState.error && setWeatherState({...weatherState, error: true})
         const { value } = e.target
         setCityName(value)
         subject.next(value)
@@ -29,15 +40,15 @@ const WeatherWrapper: FC = () => {
             name[0].toUpperCase()}`
         await fetch(URL)
             .then(res => {
-                setLoadnig(false)
+                setWeatherState({...weatherState, loading: false})
                 if (!res.ok) {
-                    setWeather({})
+                    setWeatherState({...weatherState, error: true, weather: {}})
                     throw new Error('Wrong city');
                 }
                 return res;
             })
             .then(res => res.json())
-            .then(res => setWeather(res))
+            .then(res => setWeatherState({...weatherState, weather: res}))
             .catch((err) => {
                 console.log(err)
             })
@@ -45,21 +56,24 @@ const WeatherWrapper: FC = () => {
 
     useEffect(() => {
         subscription.subscribe(val => {
-            if(val.length > 0) {
+            if (val.length > 0) {
                 getWeather(val)
-                setLoadnig(true)
+                setWeatherState({...weatherState, loading: true})
             }
-            else{
-                setWeather({})
+            else {
+                setWeatherState({...weatherState, weather: {}})
             }
         })
         return () => subject.unsubscribe()
     }, [])
 
+    const {loading, weather, error} = weatherState
+
     return (
         <>
-            {loadnig && 'Loading...'}
-            {weather?.main?.temp && <h1>{weather.name}: {weather.main.temp}</h1>}
+            {loading && 'Loading...'}
+            {error && "There's no such a city"}
+            {weather.name && <h1>{weather.name}: {weather.main.temp}</h1>}
             <Browser cityName={cityName} handleInputChange={handleInput} />
         </>
     );
