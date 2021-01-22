9bf4ccdc651a1160e0720f3a7e74b6197f746189
blazej-k <blazejkania1@gmail.com>
2021-01-22 19:14:01 +0100

    reduce requests by rxjs an add loader

--- src/components/weatherComponents/WeatherWrapper.tsx
+++ src/components/weatherComponents/WeatherWrapper.tsx
@@ -1,24 +1,35 @@
 import React, { ChangeEvent, FC, useEffect, useState } from 'react'
 import * as env from 'process'
 import Browser from './search/Browser'
+import {Subject} from 'rxjs'
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+const subject = new Subject<string>();
+const subscription = subject.pipe(
+    debounceTime(800),
+    distinctUntilChanged()
+)
 
 
 const WeatherWrapper: FC = () => {
 
     const [cityName, setCityName] = useState('')
     const [weather, setWeather] = useState<any>({})
+    const [loadnig, setLoadnig] = useState(false)
     let ENDPOINT = process.env.API_KEY
 
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target
         setCityName(value)
+        subject.next(value)
     }
 
-    const getWeather = async () => {
-        const URL = `${ENDPOINT}${cityName.length > 1 ? cityName[0].toUpperCase() + cityName.slice(1, cityName.length) :
-            cityName[0].toUpperCase()}`
+    const getWeather = async (name: string) => {
+        const URL = `${ENDPOINT}${name.length > 1 ? name[0].toUpperCase() + name.slice(1, name.length) :
+            name[0].toUpperCase()}`
         await fetch(URL)
             .then(res => {
+                setLoadnig(false)
                 if (!res.ok) {
                     setWeather({})
                     throw new Error('Wrong city');
@@ -33,13 +44,23 @@ const WeatherWrapper: FC = () => {
     }
 
     useEffect(() => {
-        cityName.length > 0 ? getWeather() : setWeather({})
-    }, [cityName])
+        subscription.subscribe(val => {
+            if(val.length > 0) {
+                getWeather(val)
+                setLoadnig(true)
+            }
+            else{
+                setWeather({})
+            }
+        })
+        return () => subject.unsubscribe()
+    }, [])
 
     return (
         <>
-            <Browser cityName={cityName} handleInputChange={handleInput} />
+            {loadnig && 'Loading...'}
             {weather?.main?.temp && <h1>{weather.name}: {weather.main.temp}</h1>}
+            <Browser cityName={cityName} handleInputChange={handleInput} />
         </>
     );
 }
