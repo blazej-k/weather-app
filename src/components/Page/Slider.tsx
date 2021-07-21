65c58b6e38fc48284924d86d7d29e7a0eefc9774
blazej-k <blazejkania1@gmail.com>
2021-07-21 17:14:21 +0200

    show loader(with delay) when slider images aren't ready

--- src/components/Page/Slider.tsx
+++ src/components/Page/Slider.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo, useLayoutEffect, useState } from 'react'
+import React, { FC, memo, useEffect, useState } from 'react'
 import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/scss/image-gallery.scss"
 
@@ -11,40 +11,52 @@ interface ImagesAdresses {
 const Slider: FC = () => {
 
     const [images, setImages] = useState<ImagesAdresses[]>([])
+    const [showSlider, setShowSlider] = useState(false)
 
-    useLayoutEffect(() => {
+    const getSlides = async () => {
         const photoNumbers: number[] = []
         const imagesArr: ImagesAdresses[] = []
         for (let i = 0; i < 5;) {
             const number = Math.floor(Math.random() * 8) + 1
             if (photoNumbers.indexOf(number) === -1) {
                 photoNumbers.push(number)
-                import(`../../assets/images/slider/img${number}.jpg`)
+                await import(`../../assets/images/slider/img${number}.jpg`)
                     .then(res => imagesArr.push({ original: res.default, originalAlt: 'slider-image' }))
-                    .then(res => res === 5 && setImages(imagesArr))
                     .then(() => i++)
-                i++
             }
         }
-    }, []) 
+        setImages(imagesArr)
+    }
+
+    useEffect(() => {
+        getSlides()
+    }, [])
 
     return (
         <>
-            {images.length === 5 && <div className="slider" data-aos="fade-up" data-aos-duration="700">
-                <ImageGallery
-                    items={images}
-                    infinite={true}
-                    showThumbnails={false}
-                    autoPlay={true}
-                    showBullets={true}
-                    showFullscreenButton={false}
-                    showNav={false}
-                    showPlayButton={false}
-                    slideInterval={6000}
-                    slideDuration={350}
-                    lazyLoad={true}
-                />
-            </div>}
+            <div className="slider">
+                {!showSlider && 
+                    <div className='loader-wrapper'>
+                        <div className="loader"></div>
+                    </div>
+                }
+                <div style={{ opacity: showSlider ? 1 : 0 }}>
+                    <ImageGallery
+                        items={images}
+                        infinite={true}
+                        showThumbnails={false}
+                        autoPlay={true}
+                        showBullets={true}
+                        showFullscreenButton={false}
+                        showNav={false}
+                        showPlayButton={false}
+                        slideInterval={6000}
+                        slideDuration={350}
+                        lazyLoad={true}
+                        onImageLoad={() => setShowSlider(true)}
+                    />
+                </div>
+            </div>
         </>
     );
 }
